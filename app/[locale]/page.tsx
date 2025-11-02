38a28fae0d4ccab9cf29b1bf0275da0b59e61298
Tawhid-200 <snburgs4@gmail.com>
2025-11-02 23:11:42 +0600

    feat: add pnpm dependency to package-lock.json

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,16 +2,16 @@
 import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    const getBlogInfo = async () => {
-      const res = await fetch(
-        `/api/blogs/c0667659-7de2-4ff3-92d5-88ddef64c137`
-      );
-      const data = await res.json();
-      console.log(data.blog);
-    };
-    getBlogInfo();
-  }, []);
+  // useEffect(() => {
+  //   const getBlogInfo = async () => {
+  //     const res = await fetch(
+  //       `/api/blogs/c0667659-7de2-4ff3-92d5-88ddef64c137`
+  //     );
+  //     const data = await res.json();
+  //     console.log(data.blog);
+  //   };
+  //   getBlogInfo();
+  // }, []);
 
   return (
     <main className="p-8">
