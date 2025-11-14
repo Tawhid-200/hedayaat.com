56f55953ecabaa9e6d4722dc26ba4678f34b77ff
Tawhid-200 <snburgs4@gmail.com>
2025-11-14 11:30:03 +0600

    fix: simplify API fetch URL in SurahList component

--- components/layouts/SurahList.tsx
+++ components/layouts/SurahList.tsx
@@ -5,9 +5,7 @@ import { SurahCard } from "../ui/surahlist/Surah_Card";
 
 export const SurahList = async () => {
   cacheLife("yearly");
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SITE_URL || ""}/api/quran/chapters`
-  );
+  const res = await fetch(`/api/quran/chapters`);
   const data = await res.json();
   console.log(data);
   return <div className="w-full lg:p-8 p-0">{<SurahCard surahs={data} />}</div>;
