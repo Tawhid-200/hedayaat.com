b8d7e0f46e41a4f7eed50d121cc4123558d69a93
Tawhid-200 <snburgs4@gmail.com>
2025-11-14 11:27:38 +0600

    fix: update API fetch URL to remove Vercel environment variable fallback

--- components/layouts/SurahList.tsx
+++ components/layouts/SurahList.tsx
@@ -6,9 +6,7 @@ import { SurahCard } from "../ui/surahlist/Surah_Card";
 export const SurahList = async () => {
   cacheLife("yearly");
   const res = await fetch(
-    `${
-      process.env.NEXT_PUBLIC_SITE_URL || process.env.NEXT_PUBLIC_VERCEL_URL
-    }/api/quran/chapters`
+    `${process.env.NEXT_PUBLIC_SITE_URL || ""}/api/quran/chapters`
   );
   const data = await res.json();
   console.log(data);
