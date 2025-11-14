0cc83fcd8c2b6a4238996bc589ece9470350a268
Tawhid-200 <snburgs4@gmail.com>
2025-11-14 11:36:12 +0600

    fix: refactor SurahList to use QuranClient for fetching chapters

--- components/layouts/SurahList.tsx
+++ components/layouts/SurahList.tsx
@@ -2,11 +2,19 @@
 
 import { cacheLife } from "next/cache";
 import { SurahCard } from "../ui/surahlist/Surah_Card";
+import { Language, QuranClient } from "@quranjs/api";
+
+export const quran = new QuranClient({
+  clientId: process.env.QURAN_CLIENT_ID!,
+  clientSecret: process.env.QURAN_CLIENT_SECRET!,
+  defaults: {
+    language: Language.ENGLISH,
+  },
+});
 
 export const SurahList = async () => {
   cacheLife("yearly");
-  const res = await fetch(`/api/quran/chapters`);
-  const data = await res.json();
+  const data = await quran.chapters.findAll();
   console.log(data);
   return <div className="w-full lg:p-8 p-0">{<SurahCard surahs={data} />}</div>;
 };
